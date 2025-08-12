c88372aef15eb9ae0cc391cc9311d2c1a5b2e1d7
ndekey <dekeyzer.n@outlook.com>
2025-08-12 19:23:28 +0200

    finised C00/ex01

--- C00/ex01/ft_print_alphabet.c
+++ C00/ex01/ft_print_alphabet.c
@@ -6,7 +6,19 @@
 /*   By: nxte <nxte@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 19:16:09 by nxte              #+#    #+#             */
-/*   Updated: 2025/08/12 19:16:10 by nxte             ###   ########.fr       */
+/*   Updated: 2025/08/12 19:23:08 by nxte             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
+void	ft_print_alphabet(void)
+{
+	write(1, "abcdefghijklmnopqrstuvwxyz", 27);
+}
+
+// int main(void)
+// {
+//     ft_print_alphabet();
+//     return (0);
+// }
